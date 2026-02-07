ba9607a5ce0b3f74793ab4bd432b6961b49f66f3
DAW1208 <durvawaykole12@gmail.com>
2026-02-07 18:37:30 +0530

    added new models in the model.py file in inventory

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,11 +1,53 @@
 from django.db import models
 
 # Create your models here.
-# class Categories(models.Model):
+class Category(models.Model):
+    slug = models.SlugField()
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+            return self.category_name
+
+class Product(models.Model):
+    slug = models.SlugField(unique=True)
+    product_name = models.CharField(max_length=45)
+    
+    price = models.DecimalField(max_digits=4, decimal_places=0)
+    product_class = models.CharField(max_length=15)
+    resistant = models.CharField(max_length=15)
+    is_allergic = models.CharField(max_length=50) 
+    vitality_days = models.DecimalField(max_digits=3, decimal_places=0)
+    modify_date = models.DateField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product_name
+
+#  class Product(models.Model):
 #     slug = models.SlugField()
-#     category_name = models.CharField(max_length=100)
-#     # models.DateTimeField
-#     # models.IntegerField
-#     # models.FloatField
-#     # category_name = models.ForeignKey(Categories)
-#     # modles.F
+#     product_name = models.CharField(max_length=100)
+#     modify_date = models.DateTimeField(auto_now=True)
+#     price = models.FloatField()
+#     resistant = models.IntegerField()
+#     is_allergic = models.BooleanField()
+#     vitality_days = models.IntegerField()   
+#     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+class Sales(models.Model):
+    slug = models.SlugField(unique=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    quantity = models.IntegerField()
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    sale_date = models.DateTimeField(auto_now_add=True)
+
+   
+# class sales(models.Model):
+#     slug = models.SlugField()
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     quantity = models.IntegerField()
+#     discount = models.FloatField()
+#     total_price = models.FloatField()
+#     sale_date = models.DateTimeField(auto_now_add=True)
