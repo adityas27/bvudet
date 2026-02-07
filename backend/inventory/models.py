21d994db24f2148358a200299e795a921e568d69
Aditya Singh <adityasingh27072746@gmail.com>
2026-02-07 17:20:44 +0530

    intiated inventory

---
+++ backend/inventory/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+# class Categories(models.Model):
+#     slug = models.SlugField()
+#     category_name = models.CharField(max_length=100)
+#     # models.DateTimeField
+#     # models.IntegerField
+#     # models.FloatField
+#     # category_name = models.ForeignKey(Categories)
+#     # modles.F
